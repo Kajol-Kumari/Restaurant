b9a2b23b1c6d8b1eb4f3dd54135ec6c9e6fd3740
Kajol <kajolkumarisingh222@gmail.com>
2019-05-30 14:29:58 +0530

    mod1_soln

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-(function(){
+(function() {
   'use strict';
-angular.module('Namecalc',[])
-.controller('namecalccontroller',function($scope,$filter){
-$scope.name="";
-$scope.totalvalue=0;
-$scope.displayNumeric=function()
-  {
-    var t=calcnum($scope.name);
-    $scope.totalvalue=t;
+  angular.module('LunchCheck',[])
+    .controller('LunchCheckController',LunchCheckController)
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.list="";
+    $scope.result="";
+    $scope.msg={};
+    $scope.box={};
+    $scope.box.myStyle={"width":"400px"};
+    $scope.message=function(){
+      var items=$scope.list.split(',');
+      var empty=$scope.list.split(' ');
+      var emp2 = $scope.list.split(',,');
+      var len=items.length-(empty.length+emp2.length);
+      if($scope.list.length == 0)
+      {
+        $scope.result ="Please enter data first";
+        $scope.msg.style={"color":"red"};
+        $scope.box.myStyle={"border":"2px solid red","width":"400px"};
+      }
+      else
+      {
+          if(len < 2)
+          {
+            $scope.result = "Enjoy!";
+            $scope.msg.style={"color":"green"};// message text-color
+            $scope.box.myStyle={"border":"2px solid green","width":"400px"};// text-box border
+          }
+          else
+          {
+            $scope.result = "Too much!";
+            $scope.msg.style={"color":"green"};//message text-color
+            $scope.box.myStyle={"border":"2px solid green","width":"400px"};//  text-box border
+          }
+        }
+    };
   };
-  $scope.Uprcase=function ()
-  {
-    var upcase = $filter('uppercase');
-    $scope.name = upcase($scope.name);
-  };
-function calcnum(string)
-{
-  var ts=0;
-  for(var i=0;i<string.length;i++)
-  {
-    ts+=string.charCodeAt(i);
-  }
-  return ts;
-}
-});
-
 })();
