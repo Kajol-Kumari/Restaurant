d22c19b9b48f53613e3d8aaf891a334c1432191f
Kajol <kajolkumarisingh222@gmail.com>
2019-06-07 11:50:59 +0530

    Mod2_solution

--- README.md
+++
@@ -1,2 +0,0 @@
-# courserra
-Repo for courserra
--- app.js
+++ app.js
@@ -1,40 +1,72 @@
-(function() {
+(function(){
   'use strict';
-  angular.module('LunchCheck',[])
-    .controller('LunchCheckController',LunchCheckController)
-  LunchCheckController.$inject = ['$scope'];
-  function LunchCheckController($scope) {
-    $scope.list="";
-    $scope.result="";
-    $scope.msg={};
-    $scope.box={};
-    $scope.box.myStyle={"width":"400px"};
-    $scope.message=function(){
-      var items=$scope.list.split(',');
-      var empty=$scope.list.split(' ');
-      var emp2 = $scope.list.split(',,');
-      var len=items.length-(empty.length+emp2.length);
-      if($scope.list.length == 0)
-      {
-        $scope.result ="Please enter data first";
-        $scope.msg.style={"color":"red"};
-        $scope.box.myStyle={"border":"2px solid red","width":"400px"};
-      }
-      else
-      {
-          if(len < 2)
-          {
-            $scope.result = "Enjoy!";
-            $scope.msg.style={"color":"green"};// message text-color
-            $scope.box.myStyle={"border":"2px solid green","width":"400px"};// text-box border
-          }
-          else
-          {
-            $scope.result = "Too much!";
-            $scope.msg.style={"color":"green"};//message text-color
-            $scope.box.myStyle={"border":"2px solid green","width":"400px"};//  text-box border
-          }
-        }
+  angular.module('ShoppingListCheckOff',[])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function  ToBuyController (ShoppingListCheckOffService){
+    var list1 = this;
+    list1.buyItems = ShoppingListCheckOffService.getBuyItems();
+    list1.itemName = "";
+    list1.itemQuantity = "";
+    list1.addItem= function(){
+        ShoppingListCheckOffService.addItem(list1.itemName,list1.itemQuantity);
     };
-  };
+
+    list1.addtoBoughtList =  function(itemIndex){
+    ShoppingListCheckOffService.addtoBoughtList(itemIndex);
+    };
+}
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController (ShoppingListCheckOffService){
+    var list2 = this;
+    list2.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+
+    list2.itemQuantity = "";
+    list2.itemName = "";
+
+    // list2.addtoBoughtList =  function(itemIndex){
+    // ShoppingListCheckOffService.addtoBoughtList(itemIndex);
+    // };
+
+  }
+  function ShoppingListCheckOffService(){
+    var service = this;
+    var boughtItems = [];
+    service.addItem = function(itemName,quantity){
+      var item={
+        name: itemName,
+        quantity :quantity
+      };
+      buyItems.push(item);
+  }
+
+    var buyItems =[ {name: "cookies", quantity: '10'},
+      {name: "Milk",quantity: ' 4 Bottles'},
+      {name: "Cereal",quantity: '4 boxes'},
+      {name: "Chocolates",quantity: '5 Packets'},
+      {name: "Coke",quantity: '2 bottles'}
+    ];
+
+    service.addtoBoughtList = function(itemIndex){
+     boughtItems.push(buyItems[itemIndex]);
+       buyItems.splice(itemIndex, 1);
+    };
+
+    // service.moveElement = function (index, buyItems, boughtItems) {
+    //   boughtItems.push(buyItems[index]);
+    //   buyItems.splice(index, 1);
+    // }
+
+    service.getBuyItems = function(){
+      return buyItems;
+    };
+
+    service.getBoughtItems = function(){
+      return boughtItems;
+    };
+  }
 })();
